bf00a6b4831783ffd0456a3dd5bd5b953d91f0ce
Peter N Jenkin <peternjenkin@gmail.com>
2019-07-23 11:43:38 +0100

    6-31 Line altered to use functional RSS feed

--- Finance/ViewModel/MainVM.cs
+++ Finance/ViewModel/MainVM.cs
@@ -29,8 +29,8 @@ namespace Finance.ViewModel
 
             using (WebClient client = new WebClient())
             {
-                string xml = Encoding.Default.GetString(client.DownloadData("https://www.finzen.mx/blog-feed.xml"));
-                //string xml = Encoding.Default.GetString(client.DownloadData("https://www.nasa.gov/rss/dyn/educationnews.rss"));
+                //string xml = Encoding.Default.GetString(client.DownloadData("https://www.finzen.mx/blog-feed.xml"));
+                string xml = Encoding.Default.GetString(client.DownloadData("https://www.nasa.gov/rss/dyn/educationnews.rss"));
                 using (Stream reader = new MemoryStream(Encoding.UTF8.GetBytes(xml)))
                 {
                     Blog = (Posts)serializer.Deserialize(reader);
